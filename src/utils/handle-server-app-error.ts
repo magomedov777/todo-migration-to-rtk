636abe0dee231d456754676e8b183d751d688e57
Arkadiy <alibullterrier@gmail.com>
2023-10-16 13:28:15 +0300

    update handle-server-app-error utills func, rewrite js doc, conduct setting

--- src/utils/handle-server-app-error.ts
+++ src/utils/handle-server-app-error.ts
@@ -3,19 +3,22 @@ import { Dispatch } from "redux";
 import { appActions } from "app/app.reducer";
 
 /**
- * Handles server application error by dispatching appropriate actions.
- *
- * @template D - The type of the data received from the server response.
- * @param {ResponseType<D>} data - The server response containing data and error messages.
- * @param {Dispatch} dispatch - The Redux dispatch function.
- * @returns {void}
- */
+Handles server application errors and dispatches actions accordingly.
+@template D - The type of data returned from the server
+@param {ResponseType<D>} data - The response data from the server
+@param {Dispatch} dispatch - The Redux dispatch function
+@param {boolean} [showError=true] - Indicates whether to show the error message or not
+@returns {void}
+**/
 
-export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch) => {
-  if (data.messages.length) {
-    dispatch(appActions.setAppError({ error: data.messages[0] }));
-  } else {
-    dispatch(appActions.setAppError({ error: "Some error occurred" }));
+export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch, showError: boolean = true) => {
+  if (showError) {
+    if (data.messages.length) {
+      dispatch(appActions.setAppError({ error: data.messages[0] }));
+    } else {
+      dispatch(appActions.setAppError({ error: "Some error occurred" }));
+    }
   }
+
   dispatch(appActions.setAppStatus({ status: "failed" }));
 };
