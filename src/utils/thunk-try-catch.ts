76d8cec647b2fbac55096bcaf4677a1b8d7ac4c2
Arkadiy <alibullterrier@gmail.com>
2023-10-17 05:29:19 +0300

    create: js doc for thunk-try-catch uttils func

--- src/utils/thunk-try-catch.ts
+++ src/utils/thunk-try-catch.ts
@@ -4,6 +4,15 @@ import { appActions } from "app/app.reducer";
 import { ResponseType } from "common/types";
 import { handleServerNetworkError } from "./handle-server-network-error";
 
+/**
+
+Executes a thunk function with error handling and status updates.
+@template T - The type of the result returned by the logic function.
+@param {BaseThunkAPI<AppRootStateType, unknown, AppDispatch, null | ResponseType>} thunkAPI - The base ThunkAPI object.
+@param {() => Promise<T>} logic - The logic function to execute.
+@returns {Promise<T | ReturnType<typeof thunkAPI.rejectWithValue>>} - The result of the logic function or a rejected value.
+**/
+
 export const thunkTryCatch = async <T>(
   thunkAPI: BaseThunkAPI<AppRootStateType, unknown, AppDispatch, null | ResponseType>,
   logic: () => Promise<T>
