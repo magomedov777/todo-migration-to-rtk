32217fe48962be311d94b8878425338297a3c54a
Arkadiy <alibullterrier@gmail.com>
2023-10-18 12:34:51 +0300

    create: taskApi, update navigation, update imports

--- src/features/TodolistsList/todolist.api.ts
+++ src/features/TodolistsList/todolist.api.ts
@@ -22,18 +22,6 @@ export const todolistsAPI = {
     const promise = instance.put<ResponseType>(`todo-lists/${id}`, { title: title });
     return promise;
   },
-  getTasks(todolistId: string) {
-    return instance.get<GetTasksResponse>(`todo-lists/${todolistId}/tasks`);
-  },
-  deleteTask(todolistId: string, taskId: string) {
-    return instance.delete<ResponseType>(`todo-lists/${todolistId}/tasks/${taskId}`);
-  },
-  createTask(arg: AddTaskArgType) {
-    return instance.post<ResponseType<{ item: TaskType }>>(`todo-lists/${arg.todolistId}/tasks`, { title: arg.title });
-  },
-  updateTask(todolistId: string, taskId: string, model: UpdateTaskModelType) {
-    return instance.put<ResponseType<TaskType>>(`todo-lists/${todolistId}/tasks/${taskId}`, model);
-  },
 };
 
 export type TodolistType = {
@@ -41,43 +29,6 @@ export type TodolistType = {
   title: string;
   addedDate: string;
   order: number;
-};
-
-export type TaskType = {
-  description: string;
-  title: string;
-  status: TaskStatuses;
-  priority: TaskPriorities;
-  startDate: string;
-  deadline: string;
-  id: string;
-  todoListId: string;
-  order: number;
-  addedDate: string;
-};
-export type UpdateTaskModelType = {
-  title: string;
-  description: string;
-  status: TaskStatuses;
-  priority: TaskPriorities;
-  startDate: string;
-  deadline: string;
-};
-type GetTasksResponse = {
-  error: string | null;
-  totalCount: number;
-  items: TaskType[];
-};
-
-export type AddTaskArgType = {
-  todolistId: string;
-  title: string;
-};
-
-export type UpdateTaskArgType = {
-  taskId: string;
-  domainModel: UpdateDomainTaskModelType;
-  todolistId: string;
 };
 
 export type TodoArgType = {
